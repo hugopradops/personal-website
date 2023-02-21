3bbd5b84132b2a01fc68464f1f415cec441f52b0
hugopradops <hugop.dev@gmail.com>
2023-02-21 14:10:53 -0500

    rasp 1

--- src/App.jsx
+++ src/App.jsx
@@ -4,40 +4,24 @@ import { AboutMe } from './components/AboutMe'
 
 import Lottie from 'lottie-react';
 import Guy from "./animation.json"
+import styles from "./App.module.css";
 
 const styleAni = {
   height:500,
   width: 700,
   margin: 10,
   padding: 10,
-  display: 'block',
+  display: 'inline-block',
 };
-
-
-// import { Animation } from './components/Animation'
-
-// var animation = bodymovin.loadAnimation({
-
-//   container: document.getElementById(ani),
-//   path:'./componets/watch-computer-animation.json',
-//   render: 'svg',
-//   loop: true,
-//   autoplay: true,
-//   name: 'computer guy'
-// });
-
-
-
-
 
 export function App() {
   return (
     <div>
       <Header/>
-        <main>
-          <Lottie loop ={true} style={styleAni} animationData={Guy} />
-          <AboutMe/>
-        </main>
+      <main className={styles.main}>
+        <Lottie loop ={true} style={styleAni} animationData={Guy} />
+        <AboutMe/>
+      </main>
     </div>
   )
 }
